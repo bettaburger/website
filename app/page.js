bd5e0e0103095a3ac1c45b68818c9930182279a9
stephanie <stephanieha2021@gmail.com>
2025-10-29 19:26:27 -0400

    page load header

--- app/page.js
+++ app/page.js
@@ -3,6 +3,15 @@ import './page.css'
 export default function Home() {
   return (
     <div className="page">
+      <header className="header">
+        <h1>Stephanie Ha</h1>
+        <h2>Student at University of Massachusetts Boston</h2>
+      </header>
+
+      <main className="main">
+        {/* Your main content goes here */}
+        <p>Welcome to my page!</p>
+      </main>
     </div>
   );
 }
